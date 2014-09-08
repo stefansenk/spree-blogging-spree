8028b2383bbc50d6a8c786364da0da316ddcf385
forever sumit <forever.sumitsharma@gmail.com>
2014-09-08 11:39:26 +0530

    show uploaded blog image at front end'

--- app/models/spree/blog_entry_image.rb
+++ app/models/spree/blog_entry_image.rb
@@ -1,7 +1,7 @@
 class Spree::BlogEntryImage < Spree::Image
-  has_attached_file :attachment, 
-     :styles => {:mini => '48x48#', :normal => '200x200>', :large => '600x600>'},
+  has_attached_file :attachment,
+     :styles => {:mini => '48x48#', :normal => '150x150#', :large => '400x400>'},
      :default_style => :large,
      :url => "/assets/blog_entry_images/:id/:style/:basename.:extension",
-     :path => ":rails_root/public/assets/blog_entry_images/:id/:style/:basename.:extension"  
+     :path => ":rails_root/public/assets/blog_entry_images/:id/:style/:basename.:extension"
 end

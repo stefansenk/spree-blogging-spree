d8320dc1353093e35ea9a88fe743b281760fa512
alex <alesssandro@ukr.net>
2015-02-18 16:14:00 +0200

    fix some troubles on admin blog index page

--- app/helpers/spree/admin/navigation_helper_decorator.rb
+++ app/helpers/spree/admin/navigation_helper_decorator.rb
@@ -1,7 +1,9 @@
 Spree::Admin::NavigationHelper.class_eval do
   def link_to_blog_in_store(resource, options={})
     url = options[:url] || blog_entry_permalink(resource)
+    name = options[:name] || Spree.t('admin.view_in_store')
     #options[:data] = {:action => 'show'}
-    link_to_with_icon('icon-view', Spree.t(:view_in_store), url, options)
+    options[:class] = "btn btn-primary btn-sm"
+    link_to_with_icon('eye-open', name, url, options)
   end
 end

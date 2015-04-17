0e8d1e70f18f58013dfdaf9e03f10a119e5fd780
GarPit <garik.piton@gmail.com>
2015-04-17 18:06:36 +0300

    support for spree 3.0

--- app/overrides/add_blog_admin_menu_tab.rb
+++ app/overrides/add_blog_admin_menu_tab.rb
@@ -1,5 +1,5 @@
-Deface::Override.new(:virtual_path => "spree/admin/shared/_menu",
+Deface::Override.new(:virtual_path => "spree/layouts/admin",
                      :name => "blog_admin_tabs",
-                     :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
-                     :text => "<%= tab(:blog_entries, :label => 'Blog', :url => spree.admin_blog_entries_path, :icon => 'file') if can? :manage, Spree::BlogEntry %>",
+                     :insert_bottom => "#main-sidebar",
+                     :partial => 'spree/admin/shared/blog_sidebar_menu',
                      :disabled => false)
